3a6ea64a64e2317770f068f48e88fa56a4c0bef6
Jorge Leon <leon.edu@outlook.com>
2018-07-03 21:47:18 -0500

    No operation

--- app/src/main/java/com/udacity/gradle/builditbigger/EndPointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndPointsAsyncTask.java
@@ -3,12 +3,14 @@ package com.udacity.gradle.builditbigger;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v4.util.Pair;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
 import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.leon.example.javajokelibrary.Joke;
 import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
 
 import java.io.IOException;
@@ -16,9 +18,10 @@ import java.io.IOException;
 public class EndPointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
 
     private static MyApi myApiService = null;
-    private Context context;
+    private Context mContext;
 
 
+    //Updated from (Pair<Context, String> to . . . Context
     @Override
     protected String doInBackground(Pair<Context, String>... params) {
         if (myApiService == null) {  // Only do this once
@@ -39,11 +42,11 @@ public class EndPointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, S
             myApiService = builder.build();
         }
 
-        context = params[0].first;
-        String name = params[0].second;
+        mContext = params[0].first;
+        //String name = params[0].second;
 
         try {
-            return myApiService.sayHi(name).execute().getData();
+            return myApiService.sayHi(Joke.getJoke()).execute().getData();
         } catch (IOException e) {
             return e.getMessage();
         }
@@ -52,8 +55,10 @@ public class EndPointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, S
     //When tasks finishes, save Joke information.
     @Override
     protected void onPostExecute(String result) {
+
+        Log.v("EndPoints", "post Execute complete");
         //TODO: Save result to String.
-        Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+        Toast.makeText(mContext, result, Toast.LENGTH_LONG).show();
     }
 }
 
--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -20,9 +20,10 @@ public class MyEndpoint {
 
     /** A simple endpoint method that takes a name and says Hi back */
     @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
+    public MyBean sayHi(@Named("name") String jokeText) {
+
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+        response.setData("Hi." + jokeText);
 
         return response;
     }
